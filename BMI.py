5edc083111e5583b5cd8d036938363c96266ebc2
ashok374 <134310976+ashok374@users.noreply.github.com>
2023-11-29 20:46:21 +0530

    BMI.py

--- BMI.py
+++ BMI.py
@@ -1,93 +1,17 @@
-from tkinter import *
-from tkinter import messagebox
+  
+the_height = float(input("Enter the height in cm: "))  
 
+the_weight = float(input("Enter the weight in kg: "))  
+ 
+the_BMI = the_weight / (the_height/100)**2  
 
-def calculate_bmi():
-    kg = int(weight_tf.get())
-    m = int(height_tf.get())/100
-    bmi = kg/(m*m)
-    bmi = round(bmi, 1)
-    bmi_index(bmi)
+print("Your Body Mass Index is", the_BMI)  
 
-def bmi_index(bmi):
-    
-    if bmi < 18.5:
-        messagebox.showinfo( f'BMI = {bmi} is Underweight')
-    elif (bmi > 18.5) and (bmi < 24.9):
-        messagebox.showinfo( f'BMI = {bmi} is Normal')
-    elif (bmi > 24.9) and (bmi < 29.9):
-        messagebox.showinfo(f'BMI = {bmi} is Overweight')
-    elif (bmi > 29.9):
-        messagebox.showinfo( f'BMI = {bmi} is Obesity') 
-    else:
-        messagebox.showerror( 'something went wrong!')   
-
-s = Tk()
-s.title('BMI CALCLUATOR')
-s.geometry('400x300')
-s.config(bg='#686e70')
-
-var = IntVar()
-
-frame = Frame(
-    s,
-    padx=10, 
-    pady=10
-)
-frame.pack(expand=True)
-
-
-age_lb = Label(
-    frame,
-    text="Enter Age (2 - 120)"
-)
-age_lb.grid(row=1, column=1)
-
-age_tf = Entry(
-    frame, 
-)
-age_tf.grid(row=1, column=2, pady=5)
-
-
-
-frame2 = Frame(
-    frame
-)
-frame2.grid(row=2, column=2, pady=5)
-
-height_lb = Label(
-    frame,
-    text="Enter Height (cm)  "
-)
-height_lb.grid(row=3, column=1)
-
-weight_lb = Label(
-    frame,
-    text="Enter Weight (kg)  ",
-
-)
-weight_lb.grid(row=4, column=1)
-
-height_tf = Entry(
-    frame,
-)
-height_tf.grid(row=3, column=2, pady=5)
-
-weight_tf = Entry(
-    frame,
-)
-weight_tf.grid(row=4, column=2, pady=5)
-
-frame3 = Frame(
-    frame
-)
-frame3.grid(row=5, columnspan=3, pady=10)
-
-cal_btn = Button(
-    frame3,
-    text='Calculate',
-    command=calculate_bmi
-)
-cal_btn.pack(side=LEFT)
-
-s.mainloop()
+if the_BMI <= 18.5:  
+    print("Oops! You are underweight.")  
+elif the_BMI <= 24.9:  
+    print("Awesome! You are healthy.")  
+elif the_BMI <= 29.9:  
+    the_print("Eee! You are over weight.")  
+else:  
+    print("Seesh! You are obese.")  
